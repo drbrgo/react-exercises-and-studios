947d6755b6f931b51a9afc97d8b65ccc9156e2db
unknown <d.b.goga@gmail.com>
2023-08-03 20:39:40 -0400

    completed first 10 steps of studio

---
+++ part1/studio/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import RecipeDescription from './components/Description.js';
+
+function App() {
+  return (
+  <div className="App">
+   <div>
+      <RecipeDescription />
+   </div>
+</div>
+
+  );
+}
+
+export default App;
---
+++ part1/studio/src/components/Description.js
@@ -0,0 +1,35 @@
+import styles from './Description.module.css';
+import React from 'react';
+
+const RecipeAuthor = () => {
+    let authorLink = "https://thewoksoflife.com/" ;
+    let authorPhoto = "https://thewoksoflife.com/wp-content/uploads/2020/10/TWOL-Headshots-Sarah2-edited-1.jpg";
+    let authorName = "Sarah, elder daughter of the Woks of Life family";
+    return (
+            <div className = {styles.recipeAuthorBlock}>
+               <img src={authorPhoto} alt = "Sarah holding knife" className={styles.imageUpdates} />
+               <div>
+                  <h3>{authorName}</h3>
+                  <a href={authorLink}>The Woks of Life</a> 
+               </div>
+            </div>
+    );
+};
+
+class RecipeDescription extends React.Component {
+    render() {
+        return (
+            <div> 
+            <div>
+               <h1>Chinese Tomato Egg Stir-Fry</h1>
+               <p>The tangy tomatoes, mixed with rich scrambled eggs, a hint of sugar, and flavorful sesame oil and Shaoxing wine all come together to create a very satisfying (and vegetarian!) dish that tastes lovely on a bed of rice.</p>
+            </div>
+            <RecipeAuthor />
+         </div>
+        )
+
+    }
+}
+
+//export default RecipeAuthor;
+export default RecipeDescription;
