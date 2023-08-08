54885bfe6345871010649fe93092ff53b0436b08
unknown <d.b.goga@gmail.com>
2023-08-07 22:01:14 -0400

    completed studio of part 2!

--- part2/studio/src/components/AuthorInfo.js
+++ part2/studio/src/components/AuthorInfo.js
@@ -1,6 +1,30 @@
+import recipedata from "./recipe.json";
+import "./styling.css";
+
 function AuthorInfo() {
-   return;
- }
+  const recipeAuthor = recipedata.map((data) => {
+    return <div key={data.name}>{data.author}</div>
+  });
+  const recipeAuthorImage = recipedata.map((data) => {
+    return(
+      <div key={data.name}>
+       <img src={data.authorImage} alt={data.author} className="authorImage" />
+     </div>
+    )
+  });
+  const recipeURL = recipedata.map((data) => {
+    return <a href={data.website}>https://thewoksoflife.com/stir-fried-tomato-and-egg/</a>
+    });
+return (
+  <div>
+    <br></br>
+   {recipeAuthorImage}
+   <br></br>
+   {recipeAuthor}
+   {recipeURL}
+  </div>
+);
+};
  
  export default AuthorInfo;
  
--- part2/studio/src/components/Ingredients.js
+++ part2/studio/src/components/Ingredients.js
@@ -1,9 +1,46 @@
+import recipedata from "./recipe.json"
+
 function IngredientList() {
-   return;
+  return (
+     <div>
+        <h3>Ingredients</h3>
+        {recipedata.map((data, name) => {
+          return (
+            <div key={data.name}>
+              {data.ingredients.map((ingredient, index) => {
+                return <li key={index}>{ingredient}</li>;
+              })}
+            </div>
+            )})}
+      </div>
+  );     
  }
  
  export default IngredientList;
  
 //import json file for the data
-//use a nested map to get inside the inner array
- 
+// //use a nested map to get inside the inner array
+// const recipeName = recipedata.map((data, recipe) => {
+//   return(
+//  <div key={recipe}>
+//   <h1>{data.name}</h1>
+// </div>
+// )
+// })
+// return <div>{recipeName}</div>
+
+// const ingredientList = recipedata.map((data, name) => {
+//   return data.ingredients;
+// })
+//   const ingredientslisted = ingredientList.map((ingredient, idx) => {
+//     return (
+//       <li key={idx}>{ingredient}</li>
+//     )
+// })
+// return (
+//   <div>
+//      <h3>Ingredients</h3>
+//      <ul>{ingredientslisted}</ul>
+//   </div>
+// );
+// }
